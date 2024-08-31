72f5f358c1fade4a202bb9a93853ae521a4600b9
lewisombaka <lewisochieng99@gmail.com>
2024-08-31 15:23:33 +0300

    number validator

---
+++ js/script/main.js
@@ -0,0 +1,18 @@
+// 2. Linking html with js
+// console.log('Loasa');
+// console.log(typeof 'loasa');
+// console.log(typeof 42);
+// console.log(typeof true);
+
+// 3. Strings
+const myVariable = 'Mathematics';
+console.log(myVariable.length);
+console.log('Every good boy does fine'.length);
+console.log(myVariable.charAt(0));
+console.log(myVariable.indexOf('M'));
+console.log(myVariable.lastIndexOf('ath'));
+console.log(myVariable.slice(5,8));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.toLowerCase());
+

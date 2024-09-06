c1fe2f0e6114fd97e8e052715b1101720474c598
lewisombaka <lewisochieng99@gmail.com>
2024-09-06 20:30:09 +0300

    number

--- js/script/main.js
+++ js/script/main.js
@@ -5,14 +5,32 @@
 // console.log(typeof true);
 
 // 3. Strings
-const myVariable = 'Mathematics';
-console.log(myVariable.length);
-console.log('Every good boy does fine'.length);
-console.log(myVariable.charAt(0));
-console.log(myVariable.indexOf('M'));
-console.log(myVariable.lastIndexOf('ath'));
-console.log(myVariable.slice(5,8));
-console.log(myVariable.toUpperCase());
-console.log(myVariable.toLowerCase());
-console.log(myVariable.toLowerCase());
+// const myVariable = 'Mathematics';
+// console.log(myVariable.length);
+// console.log('Every good boy does fine'.length);
+// console.log(myVariable.charAt(0));
+// console.log(myVariable.indexOf('M'));
+// console.log(myVariable.lastIndexOf('ath'));
+// console.log(myVariable.slice(5,8));
+// console.log(myVariable.toUpperCase());
+// console.log(myVariable.toLowerCase());
+// console.log(myVariable.toLowerCase());
+// console.log(myVariable.includes('theme')); 
+// console.log(myVariable.split('a'));
 
+// 4. NUmbers
+const myNumber = 42;
+const myFloat = 43.023;
+const myString = '47fg56';
+const myCleanString = '47';
+// console.log(myNumber.isInteger());
+// const a= Number.parseFloat(myFloat);
+// console.log(typeof(a));
+// console.log(myFloat.toFixed(2)); - states the number of decimal places
+// console.log(Number.parseInt(myString)); - brings out the integers
+// console.log(Number(myCleanString));
+
+// console.log(Number(true));
+// console.log(Number(false));
+
+// console.log(Number.isInteger(myNumber));

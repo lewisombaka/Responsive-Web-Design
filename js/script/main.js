5abdd486885d18fe6153ea6363f7c8f548030e36
lewisombaka <lewisochieng99@gmail.com>
2024-09-06 21:48:59 +0300

    math methods

--- js/script/main.js
+++ js/script/main.js
@@ -19,18 +19,37 @@
 // console.log(myVariable.split('a'));
 
 // 4. NUmbers
-const myNumber = 42;
-const myFloat = 43.023;
-const myString = '47fg56';
-const myCleanString = '47';
+// const myNumber = 42;
+// const myFloat = 43.023;
+// const myString = '47fg56';
+// const myCleanString = '47';
+// const string = 'Lewis'
 // console.log(myNumber.isInteger());
 // const a= Number.parseFloat(myFloat);
 // console.log(typeof(a));
 // console.log(myFloat.toFixed(2)); - states the number of decimal places
 // console.log(Number.parseInt(myString)); - brings out the integers
 // console.log(Number(myCleanString));
-
+// console.log(isNaN(myNumber)); - states whether the parsed arguement is not a number
 // console.log(Number(true));
 // console.log(Number(false));
 
 // console.log(Number.isInteger(myNumber));
+
+// 5. Math Methods
+
+console.log(Math.PI);
+console.log (Math.trunc(Math.PI)); 
+
+console.log (Math.round(Math.PI));
+console.log (Math.ceil(Math.PI));
+console.log (Math.floor(Math.PI));
+console.log (Math.pow(2,4));
+console.log (Math.min(2, 4, 6, 0));
+console.log (Math.max(2, 4, 6, 0));
+console.log (Math.random());
+console.log (Math.random());
+console.log (Math.random());
+console.log (Math.floor((Math.random() * 10)+1));
+console.log (Math.floor((Math.random() * 10)+1));
+console.log (Math.floor((Math.random() * 10)+1));
